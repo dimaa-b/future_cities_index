8bfbff1a1e0fc43fd34c3eb975d963f7f4a78000
dimaa-b <dmitrybaryshnikov1@gmail.com>
2024-10-06 06:46:16 -0400

    marker icon fixed, popup enabled, census lookup

--- future-cities-index-frontend/src/components/Map.js
+++ future-cities-index-frontend/src/components/Map.js
@@ -1,17 +1,17 @@
-import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+import { useState, useEffect } from "react";
+import { MapContainer, TileLayer, useMapEvents, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 
 export default function MapComponent() {
-    const position = [37.0902, -95.7129];
+    const [clickedPosition, setClickedPosition] = useState([0, 0]);
+    const [clickedPositionData, setClickedPositionData] = useState(null);
+
+    const initPosition = [37.0902, -95.7129];
     const mapStyle = { height: "100vh" };
 
-    const handleMapClick = (e) => {
-        const { lat, lng } = e.latlng;
-
-        // Construct WKT point format
-        const wkt = `POINT(${lng} ${lat})`; // WKT format uses "lng lat" (long-lat order)
-
-        // API call to /census/us/tracts/geometry
+    useEffect(() => {
+        const wkt = `POINT(${clickedPosition[1]} ${clickedPosition[0]})`; // WKT format uses "lng lat" (long-lat order)
         fetch(`http://localhost:3000/census/us/tracts/geometry?wkt=${encodeURIComponent(wkt)}`)
             .then((response) => {
                 if (!response.ok) {
@@ -22,13 +22,20 @@ export default function MapComponent() {
             .then((data) => {
                 // Handle the data returned from the API
                 // console.log('Census Tract Data:', data.censusTracts[0].tractCode);
-                alert(`Census tract data received for point: ${data.censusTracts[0].tractCode}`);
+                setClickedPositionData(data.censusTracts[0].tractCode);
             })
-        // alert(`Clicked at: ${lat}, ${lng}`);
+    }, [clickedPosition]);
+
+    const handleMapClick = (e) => {
+        const { lat, lng } = e.latlng;
+        setClickedPosition([lat, lng]);
+
+        // Construct WKT point format
+        const wkt = `POINT(${lng} ${lat})`; // WKT format uses "lng lat" (long-lat order);
     };
 
     return (
-        <MapContainer center={position} zoom={4.5} style={mapStyle} maxBounds={[[24.396308, -125.0], [49.384358, -66.93457]]}>
+        <MapContainer center={initPosition} zoom={4.5} style={mapStyle} maxBounds={[[24.396308, -125.0], [49.384358, -66.93457]]}>
             <TileLayer
                 attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -38,6 +45,12 @@ export default function MapComponent() {
                 url="http://localhost:3000/v1/census/us/tracts/tile/{z}/{x}/{y}"
             />
             <MapEventsHandler handleMapClick={handleMapClick} />
+
+            <Marker position={clickedPosition}>
+                <Popup>
+                    {clickedPositionData ? `Census Tract: ${clickedPositionData}` : 'Click on the map to get Census Tract data'}
+                </Popup>
+            </Marker>
         </MapContainer>
     );
 }
--- future-cities-index-frontend/src/index.js
+++ future-cities-index-frontend/src/index.js
@@ -5,7 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import "leaflet/dist/leaflet.css";
-// import Leaflet from "./Leaflet";
+import L from 'leaflet';
+
+// Fix the default icon paths
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
